ce08c89daae544321c9c8742ff4186d744d0cf11
Julien Rousseau <julrousseau20@gmail.com>
2020-04-18 16:39:45 -0400

    Map generation (texture)

---
+++ Assets/Scripts/Map/FalloffGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FalloffGenerator
+{
+    public static float[,] GenerateFalloffMap(int size, float falloffStartFactor)
+    {
+        float[,] map = new float[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Vector2 centre = Vector2.one * size / 2f;
+                Vector2 point = new Vector2(i, j);
+                float distance = Mathf.Clamp(Vector2.Distance(centre, point), 0, size / 2f);
+                float value = 2 * distance / (float)size;
+                map[i, j] = Evaluate(value, falloffStartFactor);
+            }
+        }
+
+        return map;
+    }
+
+    private static float Evaluate(float value, float falloffStartFactor)
+    {
+        float a = 3;
+        float b = falloffStartFactor;
+        return Mathf.Pow(value, a) / (Mathf.Pow(value, a) + Mathf.Pow(b * (1 - value), a));
+    }
+}
---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public enum DrawMode { NoiseMap, ColourMap, FalloffMap };
+    public DrawMode drawMode;
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    private float[,] falloffMap;
+    [Range(1,10)]public float falloffStartFactor = 2f;
+
+    private void Start()
+    {
+        falloffMap = FalloffGenerator.GenerateFalloffMap(mapWidth, falloffStartFactor);
+    }
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - falloffMap[x, y]);
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.FalloffMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapWidth, falloffStartFactor)));
+        }
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        falloffMap = FalloffGenerator.GenerateFalloffMap(mapWidth, falloffStartFactor);
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
+}

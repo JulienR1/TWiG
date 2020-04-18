d718366928c027e730afe40120f846ebc7cd4b78
Julien Rousseau <julrousseau20@gmail.com>
2020-04-18 01:33:05 -0400

    Plant base stats

--- Assets/Scripts/Plant/PlantStats.cs
+++ Assets/Scripts/Plant/PlantStats.cs
@@ -5,10 +5,33 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlantStats", menuName = "TWiG/Plant/Stats", order = 0)]
 public class PlantStats : ScriptableObject
 {
-    public float Health;
-    public Vector2 minMaxWater;
-    public Vector2 minMaxLight;
-    public Vector2 minMaxTemperature;
-    public float minNutrients;
+    [SerializeField] new private string name = "Plant";
+    [SerializeField] private float health = 20;
 
+    [Header("Effect stats")]
+    [SerializeField] private Vector2 minMaxWater = Vector2.zero;
+    [SerializeField] private Vector2 minMaxLight = Vector2.zero;
+    [SerializeField] private Vector2 minMaxTemperature = Vector2.zero;
+    [SerializeField] private float minNutrients = 0;
+
+    [Header("Growth stats")]
+    [SerializeField] private PlantStage[] growthStages = null;
+    [SerializeField] private int growthPerDay = 0;
+
+    public string Name { get => name; }
+    public float Health { get => health; }
+    public Vector2 MinMaxWater { get => minMaxWater; }
+    public Vector2 MinMaxLight { get => minMaxLight; }
+    public Vector2 MinMaxTemperature { get => minMaxTemperature; }
+    public float MinNutrients { get => minNutrients; }
+
+    public PlantStage[] GrowthStages { get => growthStages; }
+    public int GrowthPerDay { get => growthPerDay; }
+}
+
+[System.Serializable]
+public class PlantStage
+{
+    public Sprite sprite;
+    public Vector2Int minMaxGrowth;
 }
--- Assets/Scripts/PlantStage.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlantStage", menuName = "TWiG/Plant/Stage", order = 1)]
-public class PlantStage : ScriptableObject
-{
-
-}

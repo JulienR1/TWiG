89f061c31d46443f3365029702f542fb7bf4b369
Julien Rousseau <julrousseau20@gmail.com>
2020-04-19 01:05:07 -0400

    Remove map and button auto links

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,8 +8,9 @@ public class Game : MonoBehaviour
     public static Game instance;
 
     public TimeManager timeManager { get; private set; }
-    public MapBuilder mapBuilder { get; private set; }
     public UIManager uiManager { get; private set; }
+
+    public Menu menu;
 
     private void Awake()
     {
@@ -17,11 +18,15 @@ public class Game : MonoBehaviour
 
         List<IManager> managers = new List<IManager>();
         managers.Add(timeManager = GetComponent<TimeManager>());
-        managers.Add(mapBuilder = FindObjectOfType<MapBuilder>());
         managers.Add(uiManager = GetComponent<UIManager>());
 
         foreach (IManager manager in managers)
             manager.Initialize();
+    }
+
+    private void Start()
+    {
+        menu.Open();
     }
 
     public void GameOver()

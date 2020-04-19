e0488c308d6c350ec33ce8df167a5c7af7ce3ddd
Julien Rousseau <julrousseau20@gmail.com>
2020-04-19 13:29:24 -0400

    Player scripts

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -2,7 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerAnimator : MonoBehaviour
 {
+    public enum PlayerState { IDLE, WALKING };
+
+    private Animator animator = null;
+    private PlayerState currentState = PlayerState.IDLE;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Animate()
+    {
+        animator.SetInteger("animationState", (int)currentState);
+    }
+
+    private void ToggleAnimation(PlayerState state)
+    {
+        currentState = state;
+        Animate();
+    }
+
+    private void ClearAnimation()
+    {
+        currentState = PlayerState.IDLE;
+        Animate();
+    }
 
 }
